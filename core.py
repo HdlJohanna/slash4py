5f0c0bd5e228b7a7597c28920f2b5eb544e547b4
Johanna <92253630+HdlJohanna@users.noreply.github.com>
2022-03-21 21:22:00 +0100

    Update core.py

--- core.py
+++ core.py
@@ -8,7 +8,12 @@ class SlashBot:
         self.id = id
         self.loop = asyncio.new_event_loop()
 
-    def command(self,name:str =...,description:str=...,options:list=[],coro=None):
+    def command(self,auth,name:str =...,description:str=...,options:list=[],guild_id=None,coro=None):
+        
+        headers = {
+            "Authorization": f"Bot {token}"
+        }
+        
         json = {
             "name":name,
             "type":1,
@@ -24,20 +29,14 @@ class SlashBot:
                      "content": self.loop.run_until_complete(func)
                  }
              }
-             r = requests.post(url, json=json)
+
+            if guild_id:
+                url = f"https://discord.com/api/v8/applications/{self.id}/guilds/{guild__id}/commands"
+
+                r = requests.post(url, headers=headers, json=json)
+            else:
+                r = requests.post(self.url, headers=headers, json=json)
             self.commands.append(json)
 
         return decorator
 
-    def run(self,token,guild__id:int=None):
-        headers = {
-            "Authorization": f"Bot {token}"
-        }
-        
-        json = self.commands[0]
-        if guild__id:
-            url = f"https://discord.com/api/v8/applications/{self.id}/guilds/{guild__id}/commands"
-
-            r = requests.post(url, headers=headers, json=json)
-        else:
-            r = requests.post(self.url, headers=headers, json=json)
